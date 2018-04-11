66a21a7254fa1e5a22b8b2c1f95955e2b4ef5c1d
Ernesto Cuevas <ecuevas@vistaprint.com>
2018-04-11 13:55:28 +0200

    FizzBuzzerShould_ReturnAnEmptySetOfStrings_WhenTheNumberOfElementsRequestedIsLowerThanZero: Green

---
+++ FizzBuzzKata.UnitTests/FizzBuzzerShould.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace FizzBuzzKata.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzerShould
+    {
+        [Test]
+        public void ReturnAnEmptySetOfStrings_WhenTheNumberOfElementsRequestedIsLowerThanZero()
+        {
+            var fizzBuzzer = new FizzBuzzer();
+
+            IEnumerable<string> response = fizzBuzzer.GetStrings(-1);
+            Assert.IsNotNull(response);
+            Assert.IsEmpty(response);
+        }
+    }
+}
