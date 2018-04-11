e6e8c1492adec3042e58c34a2a5f317856b6c380
Ernesto Cuevas <ecuevas@vistaprint.com>
2018-04-11 14:30:40 +0200

    NumberFizzBuzzerShould_FizzWhenTheValueIsDivisibleByThree: Green

---
+++ FizzBuzzKata.UnitTests/NumberFizzBuzzerShould.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace FizzBuzzKata.UnitTests
+{
+    [TestFixture]
+    public class NumberFizzBuzzerShould
+    {
+        [Test]
+        public void FizzWhenTheValueIsDivisibleByThree ()
+        {
+            var numberFizzBuzzer = new NumberFizzBuzzer();
+
+            var response = numberFizzBuzzer.Stringify(3);
+            Assert.AreEqual("Fizz", response);
+        }
+    }
+}
---
+++ FizzBuzzKata/NumberFizzBuzzer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzKata
+{
+    public class NumberFizzBuzzer
+    {
+        public string Stringify(int numberToCheck)
+        {
+            return "Fizz";
+        }
+    }
+}
